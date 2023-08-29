20c71c2c1ea5726ba40d9fdb5bb7ccb1d9337d31
Elmer Mamani <elmer.mamani.urrelo@gmail.com>
2023-08-29 08:47:33 -0400

    Ejercicio terminado.

--- models/task.js
+++ models/task.js
@@ -14,29 +14,42 @@ let currentId = 1
  * Model class representing a task.
  * @class
  */
-
-/**
- * export class TaskModel {
- *
- *
+export class TaskModel {
   /**
    * Fetches all tasks.
    * @name TaskModel.getAllTasks
    * @static
    * @returns {Array<Object>} Array of tasks.
    */
+  static getAllTasks () {
+    return tasks
+  }
 
-/**
+  /**
    * Adds a new task to the list.
    * @name TaskModel.addTask
    * @static
    * @param {string} description - The description of the task.
    * @returns {Object} The new task object.
    */
+  static addTask (description) {
+    const task = {
+      id: currentId,
+      description
+    }
+    currentId += 1
+    tasks.push(task)
+    return task
+  }
 
-/**
+  /**
    * Deletes a task from the list by its ID.
    * @name TaskModel.deleteTask
    * @static
    * @param {number} id - The ID of the task to delete.
    */
+  static deleteTask (id) {
+    const taskIndex = tasks.findIndex(task => task.id === id)
+    tasks.splice(taskIndex, 1)
+  }
+}
